310ed8ee0d0a5e69c3b4b1d68c62aa114e422932
Soteras <carlosoteras@gmail.com>
2021-03-30 19:13:44 -0300

    Configure ex machina.

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Blog.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:bcrypt_elixir, "~> 2.3"},
-      {:ex_machina, "~> 2.7"},
+      {:ex_machina, "~> 2.7"}
     ]
   end
 
