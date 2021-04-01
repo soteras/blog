1db89d3730613c78d9de0593afbf55c55809155e
Soteras <carlosoteras@gmail.com>
2021-04-01 10:12:10 -0300

    Signup create and new.

---
+++ lib/blog_web/controllers/signup_controller.ex
@@ -0,0 +1,23 @@
+defmodule BlogWeb.SignupController do
+  use BlogWeb, :controller
+
+  alias Blog.Auth.Users
+  alias Blog.Auth.User
+
+  def new(conn, _params) do
+    changeset = User.create_changeset(%{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Users.create(user_params) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "User created successfully.")
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+end
--- lib/blog_web/router.ex
+++ lib/blog_web/router.ex
@@ -17,26 +17,15 @@ defmodule BlogWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/users", SignupController, only: [:new, :create]
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", BlogWeb do
-  #   pipe_through :api
+  # if Mix.env() in [:dev, :test] do
+  #   import Phoenix.LiveDashboard.Router
+
+  #   scope "/" do
+  #     pipe_through :browser
+  #     live_dashboard "/dashboard", metrics: BlogWeb.Telemetry
+  #   end
   # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: BlogWeb.Telemetry
-    end
-  end
 end
