00c7c991c651649f0a6ea0c5f65ef7855eb12e75
Soteras <carlosoteras@gmail.com>
2021-04-01 14:24:02 -0300

    Fix test.

--- test/blog_web/controllers/home_controller_test.exs
+++ test/blog_web/controllers/home_controller_test.exs
@@ -3,7 +3,11 @@ defmodule BlogWeb.HomeControllerTest do
 
   describe "index" do
     test "creates a new user when attrs are valid", %{conn: conn} do
-      conn = get(conn, Routes.home_path(conn, :index))
+      conn =
+        insert(:user)
+        |> signin_user(conn)
+        |> get(Routes.home_path(conn, :index))
+
       assert html_response(conn, 200) =~ "Home Page"
     end
   end
